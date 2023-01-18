93083a4d15472bb76a2b729f6913fb8f30e9dbfa
Eslam Shaker <binshakerr@gmail.com>
2023-01-18 02:12:12 +0200

    created bank account events

---
+++ contracts/BankAccount.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract BankAccount {
+
+    event Deposit(
+        address indexed user,
+        uint256 indexed accountId,
+        uint256 value,
+        unit256 timestamp
+    )
+
+    event RequestWithdraw(
+        address indexed user,
+        uint256 indexed accountId,
+        uint256 indexed withdrawId,
+        uint256 amount,
+        unit256 timestamp
+    )
+
+    event Withdraw(
+        uint indexed withdrawId,
+        uint timestamp
+    )
+
+    event AccountCreated(
+        address[] owners,
+        uint indexed id,
+        uint timestamp
+    )
+}
